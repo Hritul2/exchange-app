bc37a481edec8a4f81e33a394cd60f56e11b894c
Hritul <hritul.srivastava@gmail.com>
2024-10-16 12:23:36 +0530

    dockerfile update

--- exchange-backend-proxy/src/index.ts
+++ exchange-backend-proxy/src/index.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response, NextFunction } from "express";
-import { createProxyMiddleware, RequestHandler } from "http-proxy-middleware";
-
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express();
 
-const targetUrl = "https://api.backback.exchange";
+// Replace this with the target server URL
+const targetUrl = "https://api.backpack.exchange";
 
-app.use((req: Request, res: Response, next: NextFunction) => {
+// Handle CORS
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -19,11 +20,18 @@ app.use(
   createProxyMiddleware({
     target: targetUrl,
     changeOrigin: true,
-  }) as RequestHandler
+    //@ts-ignore
+    onProxyReq: (proxyReq, req, res) => {
+      // Optionally, you can modify the request here
+    },
+    //@ts-ignore
+    onProxyRes: (proxyRes, req, res) => {
+      // Optionally, you can modify the response here
+    },
+  })
 );
 
 const port = 8080;
-
 app.listen(port, () => {
-  console.log(`Proxy Server is running on port ${port}`);
+  console.log(`Proxy server running on http://localhost:${port}`);
 });
