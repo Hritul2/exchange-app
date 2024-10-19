ab9b7fbc5e1dc8584b2c95a02f5853e1acd8becb
Hritul <hritul.srivastava@gmail.com>
2024-10-19 14:04:47 +0530

    work on order route and its controller

---
+++ api/src/controllers/order.controller.ts
@@ -0,0 +1,83 @@
+import { asyncHandler } from "@/utils/asyncHandler";
+import { ApiError } from "@/utils/ApiError";
+import { ApiResponse } from "@/utils/ApiResponse";
+
+import { RedisManager } from "@/lib/RedisManager";
+import { ActionTypesEnum } from "@/utils/types";
+
+export const postOrder = asyncHandler(async (req, res) => {
+  const { market, price, quantity, side, userId } = req.body;
+  if (!market) {
+    throw new ApiError(400, "Market is required");
+  }
+  if (!price) {
+    throw new ApiError(400, "Price is required");
+  }
+  if (!quantity) {
+    throw new ApiError(400, "Quantity is required");
+  }
+  if (!side) {
+    throw new ApiError(400, "Side is required");
+  }
+  if (!userId) {
+    throw new ApiError(400, "User ID is required");
+  }
+
+  const response = await RedisManager.getInstance().sendAndAwait({
+    type: ActionTypesEnum.CREATE_ORDER,
+    data: {
+      market,
+      price,
+      quantity,
+      side,
+      userId,
+    },
+  });
+
+  if (!response) {
+    throw new ApiError(500, "Internal server error");
+  }
+  res.status(200).json(new ApiResponse(200, response.payload));
+});
+
+export const deleteOrder = asyncHandler(async (req, res) => {
+  const { orderId, market } = req.body;
+  if (!orderId) {
+    throw new ApiError(400, "Order ID is required");
+  }
+  if (!market) {
+    throw new ApiError(400, "Market is required");
+  }
+  const response = await RedisManager.getInstance().sendAndAwait({
+    type: ActionTypesEnum.CANCEL_ORDER,
+    data: {
+      orderId,
+      market,
+    },
+  });
+  if (!response) {
+    throw new ApiError(500, "Internal server error");
+  }
+  res.status(200).json(new ApiResponse(200, response.payload));
+});
+
+export const getOpenOrders = asyncHandler(async (req, res) => {
+  const { userId, market } = req.query;
+  if (!userId) {
+    throw new ApiError(400, "User ID is required");
+  }
+  if (!market) {
+    throw new ApiError(400, "Market is required");
+  }
+  const response = await RedisManager.getInstance().sendAndAwait({
+    type: ActionTypesEnum.GET_OPEN_ORDERS,
+    data: {
+      userId: userId as string,
+      market: market as string,
+    },
+  });
+  if (!response) {
+    throw new ApiError(500, "Internal server error");
+  }
+  res.status(200).json(new ApiResponse(200, response.payload));
+});
--- api/src/routes/order.route.ts
+++ api/src/routes/order.route.ts
@@ -1,3 +1,12 @@
+import {
+  deleteOrder,
+  getOpenOrders,
+  postOrder,
+} from "@/controllers/order.controller";
 import { Router } from "express";
 
 export const orderRouter = Router();
+
+orderRouter.post("/", postOrder);
+orderRouter.delete("/", deleteOrder);
+orderRouter.get("/open", getOpenOrders);
